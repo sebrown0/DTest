b651fed02d5e86564a8244ebb857337f695b5080
Steve Brown <sbrown@dakarsoftware.com>
2021-11-05 10:23:46 +0100

    Remove getTextByValue from ComboSelect

--- src/main/java/object_models/element/ComboSelect.java
+++ src/main/java/object_models/element/ComboSelect.java
@@ -30,64 +30,10 @@ public class ComboSelect implements Control {
 		combo.click();
 	}
 	
-	public String getTextByValue() {
-		String txt = "";
-		try {
-			txt = combo.getAttribute("value").trim();			
-		} catch (Exception e) {	
-			//Nothing			
-		}
-		return txt;
-	}
 	
 	public String getText(TextSanitiser sanitiser) {
 		TextExtractor txtExt = new TextExtractor(combo, sanitiser);
 		return txtExt.getFirstOccurenceOfTextFromElement();
-//		String txt = "";
-//		
-////		System.out.println("1->" + combo.getAttribute("class")); // TODO - remove or log
-//		
-//		try {
-//			txt = combo.getText();
-//			if(txt.length() > 1) {
-//				return txt.trim();	
-//			}			
-//		} catch (Exception e) {
-//		}
-//		
-//		try {
-//			txt = combo.getAttribute("title");
-//			if(txt.length() > 1) {
-//				return txt.trim();	
-//			}			
-//		} catch (Exception e) {
-//		}
-//		
-//		try {
-//			txt = combo.getAttribute("value");
-//			if(txt.length() > 1) {
-//				return txt.trim();	
-//			}
-//		} catch (Exception e) {
-//		}
-//		
-//		try {
-//			txt = combo.getAttribute("textContent");
-//			if(txt.length() > 1) {
-//				return txt.trim();	
-//			}
-//		} catch (Exception e) {
-//		}
-//		
-////		try {
-////			List<WebElement> children = combo.findElements(By.xpath("./child::*"));
-////			for (WebElement e : children) {
-////				System.out.println("2->" + e.getAttribute("textContent")); // TODO - remove or log 	
-////				System.out.println("3->" + e.getAttribute("class")); // TODO - remove or log
-////			}
-////		}catch (Exception e) {
-////		}
-//		return txt;
 	}
 	
 	public void writeText(String txt) {
--- src/main/java/object_models/helpers/text_utils/TextExtractor.java
+++ src/main/java/object_models/helpers/text_utils/TextExtractor.java
@@ -35,10 +35,39 @@ public class TextExtractor {
 			sanitiseText();
 			if(sanitisedText.size() > 0) {
 				return sanitisedText.get(0);
-			}
-			 
+			}			 
 		}
-//		if(getTextByValue()) return sanitisedText();
+		
+		if(getTextByValue()) {
+			sanitiseText();
+			if(sanitisedText.size() > 0) {
+				return sanitisedText.get(0);
+			}			 
+		}
+		
+		if(getTextContent()) {
+			sanitiseText();
+			if(sanitisedText.size() > 0) {
+				return sanitisedText.get(0);
+			}			 
+		}
+		
+		if(getTextByTitle()) {
+			sanitiseText();
+			if(sanitisedText.size() > 0) {
+				return sanitisedText.get(0);
+			}			 
+		}
+	
+// LAST RESORT GO THRU CHILDREN
+//try {
+//List<WebElement> children = combo.findElements(By.xpath("./child::*"));
+//for (WebElement e : children) {
+//	System.out.println("2->" + e.getAttribute("textContent")); // TODO - remove or log 	
+//	System.out.println("3->" + e.getAttribute("class")); // TODO - remove or log
+//}
+//}catch (Exception e) {
+//}
 		
 		// If we've got this far no text has been found.
 		return txt;
@@ -54,16 +83,27 @@ public class TextExtractor {
 		}
 		return foundTxt;		
 	}
-	
+
 	private boolean getTextByValue() {
+		return getTextByAttribute("value");
+	}
+	
+	private boolean getTextContent() {		
+		return getTextByAttribute("textContent");
+	}
+	
+	private boolean getTextByTitle() {		
+		return getTextByAttribute("title");
+	}
+	
+	private boolean getTextByAttribute(String attrName) {
 		boolean foundTxt = false;
 		try {
-			txt = el.getAttribute("value").trim();
+			txt = el.getAttribute(attrName).trim();
 			foundTxt = true;
 		} catch (Exception e) {	
 			//Nothing			
 		}
 		return foundTxt;
 	}
-	
 }
