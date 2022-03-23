7e7efe371e47cb1faeafccbc97add524da28fb81
sebrown0 <thebrownfamily@live.co.uk>
2022-03-23 13:02:10 +0100

    Remove unwanted from xml_tests.

--- src/main/java/a/package-info.java
+++
@@ -1,9 +0,0 @@
-@XmlSchema(namespace = "sch",
-           xmlns = {@XmlNs(prefix = "tns",
-                           namespaceURI = "sch")},
-           elementFormDefault = XmlNsForm.QUALIFIED)
-package a;
-
-import jakarta.xml.bind.annotation.XmlNs;
-import jakarta.xml.bind.annotation.XmlNsForm;
-import jakarta.xml.bind.annotation.XmlSchema;
